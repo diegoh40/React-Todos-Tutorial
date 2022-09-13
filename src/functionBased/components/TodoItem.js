687a97ad8ab700eff846cf125cee8ca93ebb12db
Diego <godie8a@hotmail.com>
2022-09-12 22:04:50 -0500

    fix linters

--- src/functionBased/components/TodoItem.js
+++ src/functionBased/components/TodoItem.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React, { useState, useEffect } from 'react';
 import styles from './TodoItem.module.css';
 
